f4c5d083484afc3955d4d406ecf7fe12bb2d434a
Lachlan Munro <lachlangmunro@gmail.com>
2022-06-12 16:25:43 +1000

    Model, routes, controller in place.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Movie.destroy_all
+puts 'Creating movies...'
+
+movie_names = []
+
+100.times do
+  movie = Movie.create!(
+    title: Faker::Movie.unique.title,
+    overview: Faker::Lorem.paragraph,
+    poster_url: Faker::LoremFlickr.image(size: '50x60', search_terms: ['movies']),
+    rating: rand(0...5)
+  )
+  movie_names << movie.title
+end
+
+movie_names.each do |n|
+  puts "Created #{n}"
+end
+puts 'Finished! Creation complete.'
